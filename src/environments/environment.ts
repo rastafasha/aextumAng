ea443e88bc2d96c9db43c41a24c836cc74b58d65
Malcolm Cordova <mercadocreativo@gmail.com>
2019-10-02 21:55:19 -0400

    cambio a ssl https

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,7 @@
 
 export const environment = {
   production: true,
-  baseUrl: 'http://aextum.com/server/'
+  baseUrl: 'https://aextum.com/server/'
   // baseUrl: 'http://localhost:8888/proyectos/aextum/server/'
 };
 
